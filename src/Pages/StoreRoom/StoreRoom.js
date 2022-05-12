ab8ea7c8ef4438df5366639a8a124840fd1a126c
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-05-12 17:25:59 +0600

    add extra route

--- src/Pages/StoreRoom/StoreRoom.js
+++ src/Pages/StoreRoom/StoreRoom.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Bar, BarChart, Legend, Tooltip, XAxis, YAxis } from 'recharts';
 import PageTitle from '../Shared/PageTitle/PageTitle';
 
 const StoreRoom = () => {
+    const [storedProducts, setStoredProducts] = useState([]);
+
+
+    useEffect(() => {
+        fetch('items.json')
+            .then(res => res.json())
+            .then(data => {
+                const product = data.map(item => {
+                    const name = item.name;
+                    const quantity = parseInt(item.quantity);
+                    const newItem = {
+                        name: name,
+                       Quantity: quantity
+                    }
+                    return newItem;
+                })
+                setStoredProducts(product);
+            })
+    }, [])
     return (
-        <div>
-            <PageTitle title={'About'}></PageTitle>
-            <h1>This is About me </h1>
+        <div className='my-5'>
+            <PageTitle title={'Store Room'}></PageTitle>
+            <div className='w-75 mx-auto'>
+                <BarChart className='w-90 mx-auto text-center' width={800} height={400} data={storedProducts}>
+                    <Bar dataKey={'Quantity'} fill="#8884d8" />
+                    <XAxis dataKey={'name'}></XAxis>
+                    <YAxis></YAxis>
+                    <Tooltip></Tooltip>
+                    <Legend></Legend>
+                </BarChart>
+            </div>
         </div>
     );
 };
